2a8cd83c5eef5265eec6ca8507ea278cd945c09a
SOWMYA-A-C <ssb.soumya@gmail.com>
2021-11-27 18:24:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -82,7 +82,7 @@ function draw() {
         {
               if (ball.body.position.x < 300) 
               {
-                  score=score+500;      
+                  score=score+100;      
                   ball=null;
                   if ( count>= 5) gameState ="end";                          
               }
@@ -90,14 +90,14 @@ function draw() {
 
               else if (ball.body.position.x < 600 && ball.body.position.x > 301 ) 
               {
-                    score = score + 100;
+                    score = score + 500;
                     ball=null;
                     if ( count>= 5) gameState ="end";
 
               }
               else if (ball.body.position.x < 900 && ball.body.position.x > 601 )
               {
-                    score = score + 200;
+                    score = score + 00;
                     ball=null;
                     if ( count>= 5)  gameState ="end";
 
